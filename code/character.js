c0bf880055ad611dd3fce04b61118294ae41569f
Milk Charity <mastermilkx@gmail.com>
2018-04-07 10:09:31 -0400

    Updated sprites and code

---
+++ code/character.js
@@ -0,0 +1,16 @@
+
+//SETUP
+var size = 16;
+
+//area for collision (x and y are relative to the object starting from the top right)
+function boundArea(x, y, w, h){
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+}
+
+
+function hacker(skill){
+
+}
