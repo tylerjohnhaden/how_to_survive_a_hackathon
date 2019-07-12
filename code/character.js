53b97461d1d88f58aa0bbc7ac020eb187b36aa10
Tyler Haden <thaden@ippon.fr>
2019-07-12 19:56:09 -0400

    initial formatting

--- code/character.js
+++ code/character.js
@@ -1,90 +1,93 @@
-
-//SETUP
+// SETUP
 var size = 16;
 
-//area for collision (x and y are relative to the object starting from the top right)
-function boundArea(x, y, w, h){
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
+// area for collision (x and y are relative to the object starting from the top right)
+function boundArea(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
 }
 
 
-function getIMGNPC(name){
-  var charIMG = new Image();
-  charIMG.src = "../sprites/" + name + ".png";
-  var charReady = false;
-  charIMG.onload = function(){charReady = true;};
+function getIMGNPC(name) {
+    var charIMG = new Image();
+    charIMG.src = "../sprites/" + name + ".png";
 
-  this.img = charIMG;
-  this.ready = charReady;
+    var charReady = false;
+    charIMG.onload = function () {
+        charReady = true;
+    };
 
-}
-
-function hacker(name, x, y, classType, skillSet){
-	this.name = name;
-	this.x = x*story.size;
-	this.y = y*story.size;
-	this.classType = classType;
-	this.skillSet = skillSet;
-
-	this.width = 16;
-	this.height = 16;
-	this.dir = "north";
-	this.action = "idle";
-
-
-	//movement
-  	this.speed = 1;
-  	this.initPos = 0;
-  	this.moving = false;
-  	this.velX = 0;
- 	this.velY = 0;
-
-	//other properties
-	this.text = ["Hi! My name is " + name + "!", "I'm a " + classType + "<0>"];
-	this.text_index = 0;
-	this.interact = false;
-	this.move = "drunk_walk";
-	this.boundary;
-	this.wt = 0;
-
-	var set = new getIMGNPC(classType);
-	this.img = set.img;
-	this.ready = set.ready;
-	this.show = true;
-
+    this.img = charIMG;
+    this.ready = charReady;
 
 }
 
-function npc(x, y, text, skin){
-	this.name = "???";
-	this.x = x*story.size;
-	this.y = y*story.size;
-  	//sprite properties
-	this.width = 16;
-	this.height = 16;
-	this.dir = "north";
-	this.action = "idle";
+function hacker(name, x, y, classType, skillSet) {
+    this.name = name;
+    this.x = x * story.size;
+    this.y = y * story.size;
+    this.classType = classType;
+    this.skillSet = skillSet;
 
-	//movement
-  	this.speed = 1;
-  	this.initPos = 0;
-  	this.moving = false;
-  	this.velX = 0;
- 	this.velY = 0;
+    this.width = 16;
+    this.height = 16;
+    this.dir = "north";
+    this.action = "idle";
 
-	//other properties
-	this.text = text;
-	this.text_index = 0;
-	this.interact = false;
-	this.move = "drunk_walk";
-	this.boundary;
-	this.wt = 0;
 
-	var set = new getIMGNPC(skin);
-	this.img = set.img;
-	this.ready = set.ready;
-	this.show = true;
+    // movement
+    this.speed = 1;
+    this.initPos = 0;
+    this.moving = false;
+    this.velX = 0;
+    this.velY = 0;
+
+    // other properties
+    this.text = ["Hi! My name is " + name + "!", "I'm a " + classType + "<0>"];
+    this.text_index = 0;
+    this.interact = false;
+    this.move = "drunk_walk";
+    this.boundary;
+    this.wt = 0;
+
+    var set = new getIMGNPC(classType);
+    this.img = set.img;
+    this.ready = set.ready;
+    this.show = true;
+
+
+}
+
+function npc(x, y, text, skin) {
+    this.name = "???";
+    this.x = x * story.size;
+    this.y = y * story.size;
+
+    // sprite properties
+    this.width = 16;
+    this.height = 16;
+    this.dir = "north";
+    this.action = "idle";
+
+    //movement
+    this.speed = 1;
+    this.initPos = 0;
+    this.moving = false;
+    this.velX = 0;
+    this.velY = 0;
+
+    //other properties
+    this.text = text;
+    this.text_index = 0;
+    this.interact = false;
+    this.move = "drunk_walk";
+    this.boundary;
+    this.wt = 0;
+
+    var set = new getIMGNPC(skin);
+    this.img = set.img;
+    this.ready = set.ready;
+    this.show = true;
 }
